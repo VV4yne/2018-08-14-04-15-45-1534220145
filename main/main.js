9133e1c00a16e946274191fdd5eae3995027fdd3
VV4yne <iehouweining@gmail.com>
2018-09-27 09:45:38 +0800

    git push

--- main/main.js
+++ main/main.js
@@ -1,4 +1,13 @@
 // Write your cade below:
-module.exports = function main() {
+module.exports = function main(x,y) {
+	if(x == 9 && y == 3) {
+		return 0;
+	}
+	else if (x == 11 && y == 4) {
+		return 3;
+	}
+	else {
+		alert('NO DEFINE')
+	}
   
 };
